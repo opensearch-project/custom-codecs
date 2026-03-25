e16481602006575498756b2efe89d895a694f0fa
Peter Zhu <zhujiaxi@amazon.com>
2026-03-25 07:39:23 -0400

    Explicitly publish custom codecs zip to mavenlocal (#321)
    * Explicitly publish custom codecs zip to mavenlocal
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Explicitly define task order so that publishPluginZipPublicationToMavenLocal must run after publishNebulaPublicationToMavenLocal
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    ---------
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -79,9 +79,12 @@ echo "COPY ${distributions}/*.zip"
 mkdir -p $OUTPUT/plugins
 cp ${distributions}/*.zip ./$OUTPUT/plugins
 
-# Publish plugin zips to maven
+# Publish jars
 ./gradlew publishToMavenLocal -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 ./gradlew publishAllPublicationsToStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
+
+# Publish zips
+./gradlew publishPluginZipPublicationToMavenLocal -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 ./gradlew publishPluginZipPublicationToZipStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 mkdir -p $OUTPUT/maven/org/opensearch
 cp -r ./build/local-staging-repo/org/opensearch/. $OUTPUT/maven/org/opensearch
